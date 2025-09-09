9d423a95d2df732245ce6d76d7d2b75f22d090f9
Thouba <thoubawaheng03@gmail.com>
2025-09-10 01:13:51 +0530

    last piece

---
+++ middlewares/example.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int sum (int x,int y,int p){
+    int xo=x;
+    int yo=y;
+    int p=x-1;
+}
+int main(){
+    int x,y;
+    scanf("%d%d",&x,&y);
+    int c= sum(x,y,0);
+    return 0;
+
+}
